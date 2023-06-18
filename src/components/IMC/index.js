0b39f810bee3742d158e8a7b82bac8c811470733
asobrabj <j.barbosasilva@yahoo.com.br>
2023-06-17 21:10:14 -0300

    Segundo commit - ajuste para não dividir por zero

--- src/components/IMC/index.js
+++ src/components/IMC/index.js
@@ -1,14 +1,21 @@
 import React, {useState} from 'react'
 import './style.css'
+import {useForm} from 'react-hook-form'
 
 function IMC () {
+
+    const { register, handleSubmit, formState: { errors } } =  useForm();
+    const  onSubmit =  data  =>  console.log(data);
+    console.log(errors);
 
     const [weight, setWeight] = useState()
     const [height, setHeight] = useState()
     const [imc, setImc] = useState(0)
 
     function calculator () {
-      setImc(parseFloat(weight / ( height * height )))
+      if ( weight > 0 && height > 0 ){ 
+      setImc(parseFloat(weight / ( height * height )).toFixed(2))
+      }
       console.log(imc)
     }
 
@@ -16,21 +23,29 @@ function IMC () {
         <div className='IMC'>
             <h2>Cálculo IMC</h2>
 
+            <form  onSubmit={handleSubmit(onSubmit)}>
             <input 
-                type='text' 
+                type='text'  
+                name='num1'
                 placeholder='digite seu peso' 
+                {...register("digite seu peso", { required: true })}
                 value={weight}
                 onChange={ e => setWeight(e.target.value)}
+                
             />
 
             <input 
-                type='text' 
+                type='text'
+                name='num2'
                 placeholder='digite sua altura'
+                {...register("digite sua altura", { required: true  })}
                 value={height} 
                 onChange={ e => setHeight(e.target.value)}
             />
 
-            <button onClick={calculator}>Calcular</button>
+            {/*<button onClick={calculator}>Calcular</button>*/}
+            <input type='submit' value="Calcular"  onClick={calculator}/>
+            </form>
 
             <p>Resultado: {imc}</p>
         </div>
